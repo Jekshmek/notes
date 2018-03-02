3d1331cdcc4b23de98a33eef8bb9e4a71061ee5d
Ihar Hancharenka <iigoncharenko@mail.ru>
2018-03-02 17:35:41 +0300

    autocommit 2018-03-02 17:35:40

--- pl/java/libfws/containers/spring/validation.java
+++
@@ -1,6 +0,0 @@
-https://docs.spring.io/spring/docs/current/spring-framework-reference/core.html#validation
-
-https://spring.io/guides/gs/validating-form-input/
-
-https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/validation/MessageCodesResolver.html
-https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/validation/DefaultMessageCodesResolver.html
